a1ddcef725cec3ee2a6b58ad90f8c40cae18525d
kayoyang <kayoyang@tencent.com>
2020-08-28 14:43:05 +0800

    update

--- UnityProj/Assets/Scripts/World.cs
+++ UnityProj/Assets/Scripts/World.cs
@@ -1,18 +1,60 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Nullspace;
 using UnityEngine;
+using Logger = Nullspace.Logger;
 
 public class World : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static void InitializeLogger(Properties cfg)
     {
-        
+        string logConfig = cfg.GetString("log_config_file", null);
+        if (logConfig != null)
+        {
+            Properties config = Properties.Create(logConfig);
+            Logger.Instance.Initialize(config);
+        }
+    }
+
+    private static void InitializeClient(Properties cfg)
+    {
+        string serverConfig = cfg.GetString("server_config_file", null);
+        if (serverConfig != null)
+        {
+            Properties config = Properties.Create(serverConfig);
+            NetworkClient.Instance.Initialize(config);
+        }
+    }
+
+    void Awake()
+    {
+        Properties cfg = Properties.Create("config.txt");
+        InitializeClient(cfg);
+        InitializeLogger(cfg);
+        TimerTaskQueue.Instance.Reset();
+        FrameTimerTaskHeap.Instance.Reset();
+        SequenceManager.Instance.Clear();
+        IntEventDispatcher.Cleanup();
+        NetworkEventHandler.Initialize();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 协议
+        NetworkEventHandler.Update();
+        TimerTaskQueue.Instance.Tick();
+        FrameTimerTaskHeap.Instance.Tick();
+        SequenceManager.Instance.Tick();
     }
+
+    void OnDestroy()
+    {
+        NetworkClient.Instance.Stop();
+        NetworkEventHandler.Clear();
+        SequenceManager.Instance.Clear();
+        TimerTaskQueue.Instance.Reset();
+        FrameTimerTaskHeap.Instance.Reset();
+        IntEventDispatcher.Cleanup();
+        Logger.Instance.Stop();
+    }
+
 }
