45aa4a9e4fdec3a6b9ea926607006a052add7556
NullspaceKay <3520170215@qq.com>
2020-08-30 22:59:48 +0800

    java

--- Protoc/buildcs.bat
+++ Protoc/buildcs.bat
@@ -1,4 +1,5 @@
 @echo off
 protoc.exe --csharp_out=../UnityProj/CommonLib/ProtobufMessage/MessageDefine *.proto
+protoc.exe --java_out=../Server/JavaServer/MessageDefine *.proto
 echo successfullly
 pause
